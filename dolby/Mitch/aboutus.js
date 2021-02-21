69d01b6b75c63104f12e48ea21ac8601d64fe624
mitchshumway <76656075+mitchshumway@users.noreply.github.com>
2021-02-20 20:49:12 -0500

    About Us Page

---
+++ dolby/Mitch/aboutus.js
@@ -0,0 +1,13 @@
+var join = document.getElementById("Join");
+var home = document.getElementById("Home");
+
+home.onclick = goHome;
+join.onclick = createAccount;
+
+function createAccount(){
+    console.log("hi");
+}
+
+function goHome(){
+    console.log("home");
+}
